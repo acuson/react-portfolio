4d4c83c115f087763aca5889af98f411810c1b21
Alyssa Cuson <acuson98@gmail.com>
2022-03-23 20:14:12 -0400

    started component

---
+++ Develop/src/components/Header.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+function Header() {
+    const [header, setHeader] = useState('Alyssa Cuson');
+
+    return(
+        <div></div>
+    );
+}
+
+export default Header;
