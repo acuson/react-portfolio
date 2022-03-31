fafe51c4199565d7ce7a05cedb53f4f5081f8c9f
Alyssa Cuson <acuson98@gmail.com>
2022-03-30 20:56:50 -0400

    fixed img references

--- src/components/Project.js
+++ src/components/Project.js
@@ -4,7 +4,7 @@ function Project () {
         <h1 class="m-5">About Me</h1>
             <div class="card bg-light border-primary m-5">
                 <div class="card-body container-fluid d-flex justify-content-between">
-                    <img src="./src/assets/Headshot.jpg" alt="Alyssa's headshot" class="w-25 m-2"/>
+                    <img src={process.env.PUBLIC_URL + '/Headshot.jpg'} alt="Alyssa's headshot" class="w-25 m-2"/>
                     <p class="lead m-2">Hi, my name is Alyssa Cuson, and I am an aspiring developer. I graduated from the University of Georgia with a degree in Economics. I started my career working for Atlanta tech startups in marketing. I then transitioned into recruiting and have experience recruiting for several industries, including IT. I am a student at Georgia Tech for their coding boot camp. My goal is to use the skills from this program to transition into an exciting career in development. This has already started as I look forward to work with Search Discovery in Atlanta as an Associate Analytics Engineer in March.</p>
                      
                 </div>
@@ -25,7 +25,7 @@ function Project () {
             <div class="card bg-light border-primary m-5">
                 <div class="card-body container-fluid d-flex row justify-content-center">
                     <h3 class="card-title">MeteoMelodies</h3>
-                    <img src="./assets/meteo-melodies.PNG" alt="Meteo Melodies landing page"/>
+                    <img src={process.env.PUBLIC_URL + '/meteo-melodies.png'} alt="Meteo Melodies landing page"/>
                     <p class="card-text m-2">A JavaScript app finding Spotify playlists based on the weather in your location.</p>
                     <a href="https://github.com/acuson/meteo-melodies" class="btn btn-success w-25 m-2">Code</a>
                     <a href="https://acuson.github.io/meteo-melodies/" class="btn btn-success w-25 m-2">App</a>
@@ -34,7 +34,7 @@ function Project () {
             <div class="card bg-light border-primary m-5">
                 <div class="card-body container-fluid d-flex row justify-content-center">
                     <h3 class="card-title">Between the Covers</h3>
-                    <img src="./build/assets/btc.png" alt="Between the Covers landing page"/>
+                    <img src={process.env.PUBLIC_URL + '/btc.png'} alt="Between the Covers landing page"/>
                     <p class="card-text m-2">A FullStack app where users can explore and create book clubs.</p>
                     <a href="https://github.com/acuson/between-the-covers" class="btn btn-success w-25 m-2">Code</a>
                     <a href="https://between-the-covers.herokuapp.com/" class="btn btn-success w-25 m-2">App</a>
@@ -43,7 +43,7 @@ function Project () {
             <div class="card bg-light border-primary m-5">
                 <div class="card-body container-fluid d-flex row justify-content-center">
                     <h3 class="card-title">Work Day Scheduler</h3>
-                    <img src="./assets/wds.PNG" alt="Work day Scheduler landing page"/>
+                    <img src={process.env.PUBLIC_URL + '/wds.png'} alt="Work day Scheduler landing page"/>
                     <p class="card-text m-2">A JavaScript app where a user can plan their day in a 15 hour span.</p>
                     <a href="https://github.com/acuson/work-day-scheduler" class="btn btn-success w-25 m-2">Code</a>
                     <a href="https://acuson.github.io/work-day-scheduler/" class="btn btn-success w-25 m-2">App</a>
